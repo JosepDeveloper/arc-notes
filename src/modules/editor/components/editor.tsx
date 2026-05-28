376654225cba84462257d084e38cf507397306df
Jose Manuel Oviedo <thejm1133@gmail.com>
2026-05-28 13:32:06 -0400

    refactor: simplify editor component by using useMemo and removing state management

--- src/modules/editor/components/editor.tsx
+++ src/modules/editor/components/editor.tsx
@@ -1,25 +1,17 @@
 import YooptaEditor, { createYooptaEditor } from "@yoopta/editor"
 import { Bold, Italic } from "@yoopta/marks"
 import Paragraph from "@yoopta/paragraph"
-import { useEffect, useState } from "react"
+import { useMemo } from "react"
 
 const plugins = [Paragraph]
 const marks = [Bold, Italic]
 
 export function Editor() {
-  // biome-ignore lint/suspicious/noExplicitAny: false positive
-  const [editor, setEditor] = useState<any>(null)
-
-  useEffect(() => {
-    setEditor(createYooptaEditor({ plugins, marks, value: undefined }))
-  }, [])
-
-  if (!editor) return null
+  const editor = useMemo(() => createYooptaEditor({ plugins, marks, value: undefined }), [])
 
   return (
     <YooptaEditor
       editor={editor}
-      onChange={(value) => console.log(value)}
       style={{
         paddingBottom: 100,
       }}
