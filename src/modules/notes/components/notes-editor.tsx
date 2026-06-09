c9005a15e9858fa4a44a44f6aeca1428cc029494
Jose Manuel Oviedo <thejm1133@gmail.com>
2026-06-09 11:03:50 -0400

    feat: integrate Skeleton component into NotesEditor and update NotesWorkspaces layout

--- src/modules/notes/components/notes-editor.tsx
+++ src/modules/notes/components/notes-editor.tsx
@@ -1,7 +1,13 @@
+import { Skeleton } from "@/modules/core/components/design-system/skeleton"
+
 type Props = {
   className?: string
 }
 
 export function NotesEditor({ className }: Props) {
-  return <section className={className}>notes editor</section>
+  return (
+    <section className={className}>
+      <Skeleton className="h-full w-full" />
+    </section>
+  )
 }
--- src/modules/notes/components/notes-workspaces.tsx
+++ src/modules/notes/components/notes-workspaces.tsx
@@ -1,4 +1,5 @@
 import { Separator } from "@/modules/core/components/design-system/separator"
+import { NotesEditor } from "./notes-editor"
 import { NotesSidebar } from "./notes-sidebar"
 
 type Props = {
@@ -10,9 +11,11 @@ export function NotesWorkspaces({ className }: Props) {
     <section className={`h-full grid grid-cols-[1fr_2fr] ${className || ""}`}>
       <div className="flex h-full">
         <NotesSidebar className="h-full w-full" />
-        <Separator orientation="vertical" className="mr-2" />
+        <Separator orientation="vertical" className="" />
       </div>
-      <div className="h-full w-full">notes editor</div>
+      <div className="h-full w-full">
+        <NotesEditor className="h-full w-full p-4" />
+      </div>
     </section>
   )
 }
